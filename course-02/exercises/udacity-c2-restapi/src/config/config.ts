7d0e0d31fd901743c30adbad3ca17faa39f9bf84
unknown <36091977+MassaKone@users.noreply.github.com>
2022-08-22 22:45:53 -0400

    First commit after long local development

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,5 +1,67 @@
 export const config = {
   "dev": {
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": process.env.POSTGRES_DIALECT,
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+export const config = {
+  "dev": {
+    "username": "udagram_db_admin",
+    "password": "K1llUminat!A",
+    "database": "udagrambymassadev_db",
+    "host": "udagrambymassadev.cijsjgphake1.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagrambymassa-dev-s3"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
+*/
+
+/*
+export const config = {
+  "dev": {
     "username": "",
     "password": "",
     "database": "",
@@ -20,3 +82,4 @@ export const config = {
     "dialect": "postgres"
   }
 }
+*/
